bc20d5ebb6190b20ecf564349ba750c03487ebb5
ericnwt <skilobaloba@gmail.com>
2023-09-09 15:40:32 -0300

    Adicionando link no menu

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+function message(){
+    swal("Nossa arena possui uma área de 525m² entre obstáculos e mata, por ser um CQB (Close Quarters Battle) nossos jogos serão de no mínimo 1 contra 1 e no máximo 4 contra 4 serão partidas Only pistol, traduzindo “Só Pistolas”, trazendo assim muito mais adrenalina por ser um jogo com muita rapidez, agilidade e um pouquinho de dor ao ser alvejado rsrsrsrs. Teremos 3 modos de jogos disponíveis o Mata-Mata, Capture a bandeira e o Bomb Squad!");
+}
